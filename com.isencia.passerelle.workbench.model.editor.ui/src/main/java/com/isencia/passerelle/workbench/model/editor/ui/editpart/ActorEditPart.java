df651e329ff9f02e67bd9d2b202bc8fd2f968bf9
Olof Svensson <svensson@esrf.fr>
2014-01-09 15:31:26 +0100

    DAWNSCI-814: Patch from Erwin for restoring colours of source and sink actors + yellow trigger input

--- com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/editpart/ActorEditPart.java
+++ com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/editpart/ActorEditPart.java
@@ -3,7 +3,6 @@ package com.isencia.passerelle.workbench.model.editor.ui.editpart;
 import java.util.EventObject;
 import java.util.Iterator;
 import java.util.List;
-
 import org.eclipse.draw2d.Clickable;
 import org.eclipse.draw2d.ConnectionAnchor;
 import org.eclipse.draw2d.IFigure;
@@ -20,7 +19,6 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import ptolemy.actor.Actor;
 import ptolemy.actor.TypedIOPort;
 import ptolemy.data.BooleanToken;
@@ -29,9 +27,6 @@ import ptolemy.data.expr.Parameter;
 import ptolemy.kernel.Entity;
 import ptolemy.kernel.Port;
 import ptolemy.kernel.util.Attribute;
-import ptolemy.kernel.util.ChangeListener;
-import ptolemy.kernel.util.ChangeRequest;
-
 import com.isencia.passerelle.core.ControlPort;
 import com.isencia.passerelle.core.ErrorPort;
 import com.isencia.passerelle.workbench.model.editor.ui.Activator;
@@ -45,12 +40,17 @@ import com.isencia.passerelle.workbench.model.ui.command.AttributeCommand;
 
 public class ActorEditPart extends AbstractNodeEditPart implements IActorNodeEditPart {
 
+  private static final String TRIGGER_PORT_NAME = "trigger";
+
   private final static Logger logger = LoggerFactory.getLogger(ActorEditPart.class);
 
   public final static ImageDescriptor IMAGE_DESCRIPTOR_PROPERTIES = Activator.getImageDescriptor("icons/input.gif");
 
   public final static Color COLOR_ERROR_PORT = new Color(null, 192, 20, 20);
   public final static Color COLOR_CONTROL_PORT = new Color(null, 50, 50, 255);
+  public final static Color COLOR_TRIGGER_PORT = new Color(null, 255, 225, 20);
+  public final static Color COLOR_SOURCE_ACTOR = new Color(null, 138, 226, 52);
+  public final static Color COLOR_SINK_ACTOR = new Color(null,252,175,62);
 
   public final static ImageDescriptor IMAGE_COMMENT = Activator.getImageDescriptor("icons/comment.png");
   public final static ImageDescriptor IMAGE_DESCRIPTOR_ACTOR = Activator.getImageDescriptor("icons/actor.gif");
@@ -66,7 +66,7 @@ public class ActorEditPart extends AbstractNodeEditPart implements IActorNodeEdi
     if (getParent() instanceof DiagramEditPart) {
       installEditPolicy(EditPolicy.GRAPHICAL_NODE_ROLE, new ActorEditPolicy(((DiagramEditPart) getParent()).getMultiPageEditorPart(), this));
     }
-    installEditPolicy(EditPolicy.COMPONENT_ROLE, new ComponentNodeDeletePolicy(getDiagram(),((DiagramEditPart) getParent()).getMultiPageEditorPart()));
+    installEditPolicy(EditPolicy.COMPONENT_ROLE, new ComponentNodeDeletePolicy(getDiagram(), ((DiagramEditPart) getParent()).getMultiPageEditorPart()));
   }
 
   /**
@@ -86,18 +86,29 @@ public class ActorEditPart extends AbstractNodeEditPart implements IActorNodeEdi
       imageDescriptor = IMAGE_DESCRIPTOR_ACTOR;
     }
     ActorFigure actorFigure = getActorFigure(actorModel.getDisplayName(), createImage(imageDescriptor), new Clickable[] {});
+    
+    boolean isSource = true;
     List<TypedIOPort> inputPortList = actorModel.inputPortList();
-    if (inputPortList != null) {
+    if (inputPortList != null && inputPortList.size()>0) {
       for (TypedIOPort inputPort : inputPortList) {
         PortFigure portFigure = actorFigure.addInput(inputPort.getName(), inputPort.getDisplayName());
         if (inputPort instanceof ErrorPort) {
           portFigure.setFillColor(COLOR_ERROR_PORT);
         } else if (inputPort instanceof ControlPort) {
           portFigure.setFillColor(COLOR_CONTROL_PORT);
+        } else if (TRIGGER_PORT_NAME.equals(inputPort.getName())) {
+          portFigure.setFillColor(COLOR_TRIGGER_PORT);
+        } else {
+          // we've got a normal input port, so the actor is not a source
+          isSource = false;
         }
       }
+    } 
+    if(isSource) {
+      actorFigure.setBackgroundColor(COLOR_SOURCE_ACTOR);
     }
-    // Add SourceConnectionAnchors
+    
+    boolean isSink = true;
     List<TypedIOPort> outputPortList = actorModel.outputPortList();
     if (outputPortList != null) {
       for (TypedIOPort outputPort : outputPortList) {
@@ -106,84 +117,89 @@ public class ActorEditPart extends AbstractNodeEditPart implements IActorNodeEdi
           portFigure.setFillColor(COLOR_ERROR_PORT);
         } else if (outputPort instanceof ControlPort) {
           portFigure.setFillColor(COLOR_CONTROL_PORT);
+        } else {
+          isSink = false;
         }
       }
     }
-    
-	// Listen to break point status if there is a break point attribute.
-	if (actorModel instanceof Entity) {
-		
-		if (isDebuggable()) {
-			updateBreakPoint(actorFigure);
+    if(isSink) {
+      actorFigure.setBackgroundColor(COLOR_SINK_ACTOR);
+    }
 
-			if (getParent() instanceof DiagramEditPart) {
-				final DiagramEditPart dep = (DiagramEditPart)getParent();
-				dep.getMultiPageEditorPart().getEditor().getEditDomain().getCommandStack().addCommandStackListener(new DebugStackNotifier(dep, actorFigure));
-			}
-		}
-	}
+    // Listen to break point status if there is a break point attribute.
+    if (actorModel instanceof Entity) {
+      if (isDebuggable()) {
+        updateBreakPoint(actorFigure);
+        if (getParent() instanceof DiagramEditPart) {
+          final DiagramEditPart dep = (DiagramEditPart) getParent();
+          dep.getMultiPageEditorPart().getEditor().getEditDomain().getCommandStack().addCommandStackListener(new DebugStackNotifier(dep, actorFigure));
+        }
+      }
+    }
     return actorFigure;
   }
-  
-	
-	private final class DebugStackNotifier implements CommandStackListener {
-		
-		private DiagramEditPart dep;
-		private ActorFigure actorFigure;
 
-		public DebugStackNotifier(DiagramEditPart dep, ActorFigure actorFigure) {
-			this.dep         = dep;
-			this.actorFigure = actorFigure;
-		}
+  private final class DebugStackNotifier implements CommandStackListener {
 
-		@Override
-		public void commandStackChanged(EventObject event) {
-			
-			CommandStack stack = (CommandStack)event.getSource();
-			Command last = stack.getUndoCommand();
-			if (!(last instanceof AttributeCommand)) return;
-			
-			if (dep.getMultiPageEditorPart().isDisposed()) {
-				stack.removeCommandStackListener(this);
-				return;
-			}
-			if (actorFigure.getParent()==null) {
-				stack.removeCommandStackListener(this);
-				return;
-			}
-			updateBreakPoint(actorFigure);
-		}
-	}
+    private DiagramEditPart dep;
+    private ActorFigure actorFigure;
 
-	private void updateBreakPoint(ActorFigure figure) {
-		final Entity entity = (Entity)getActor();
-		final Attribute att = entity.getAttribute("_break_point");
-      final boolean isBreak = getBooleanValue(att, false);
-      figure.setBreakPoint(isBreak);		
-	}
+    public DebugStackNotifier(DiagramEditPart dep, ActorFigure actorFigure) {
+      this.dep = dep;
+      this.actorFigure = actorFigure;
+    }
 
-	public boolean isDebuggable() {
-		try {
-			final Entity entity = (Entity)getActor();
-			final Attribute att = entity.getAttribute("_break_point");
-          return att!=null;
-		} catch (Throwable ne) {
-			return false;
-		}
-	}
-	
-	private static boolean getBooleanValue(Attribute att, boolean defaultValue) {
-		try {
-			if (att==null) return defaultValue;
-			if (!(att instanceof Parameter)) return defaultValue;
-			Token tok = ((Parameter)att).getToken();
-			if (!(tok instanceof BooleanToken)) return defaultValue;
-			return ((BooleanToken)tok).booleanValue();
-		} catch (Throwable ne) {
-			return defaultValue;
-		}
-	}
+    @Override
+    public void commandStackChanged(EventObject event) {
 
+      CommandStack stack = (CommandStack) event.getSource();
+      Command last = stack.getUndoCommand();
+      if (!(last instanceof AttributeCommand))
+        return;
+
+      if (dep.getMultiPageEditorPart().isDisposed()) {
+        stack.removeCommandStackListener(this);
+        return;
+      }
+      if (actorFigure.getParent() == null) {
+        stack.removeCommandStackListener(this);
+        return;
+      }
+      updateBreakPoint(actorFigure);
+    }
+  }
+
+  private void updateBreakPoint(ActorFigure figure) {
+    final Entity entity = (Entity) getActor();
+    final Attribute att = entity.getAttribute("_break_point");
+    final boolean isBreak = getBooleanValue(att, false);
+    figure.setBreakPoint(isBreak);
+  }
+
+  public boolean isDebuggable() {
+    try {
+      final Entity entity = (Entity) getActor();
+      final Attribute att = entity.getAttribute("_break_point");
+      return att != null;
+    } catch (Throwable ne) {
+      return false;
+    }
+  }
+
+  private static boolean getBooleanValue(Attribute att, boolean defaultValue) {
+    try {
+      if (att == null)
+        return defaultValue;
+      if (!(att instanceof Parameter))
+        return defaultValue;
+      Token tok = ((Parameter) att).getToken();
+      if (!(tok instanceof BooleanToken))
+        return defaultValue;
+      return ((BooleanToken) tok).booleanValue();
+    } catch (Throwable ne) {
+      return defaultValue;
+    }
+  }
 
   /**
    * Overide to return alternative actor figures
@@ -297,7 +313,6 @@ public class ActorEditPart extends AbstractNodeEditPart implements IActorNodeEdi
     }
     return null;
   }
-
 
   /**
    * Returns the connection anchor of a terget connection which is at the given point.
